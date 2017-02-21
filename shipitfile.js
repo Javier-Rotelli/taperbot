be5cd784b884e0032545f7842b6bf7f82064fa05
Javier Rotelli <javier.rotelli@gmail.com>
2017-02-21 20:20:17 -0300

    :shipit:

---
+++ shipitfile.js
@@ -0,0 +1,28 @@
+var pkg = require('./package.json');
+
+module.exports = function (shipit) {
+  require('shipit-deploy')(shipit);
+  require('shipit-npm')(shipit);
+  require('shipit-pm2')(shipit);
+  var config = require('./shipitfile.config.json')
+
+  shipit.initConfig({
+    default: {
+      workspace: '/tmp/edison-gp/backend',
+      deployTo: '/home/taperbot/code',
+      repositoryUrl: pkg.repository.url,
+      branch: 'master',
+      ignores: ['.git'],
+      keepReleases: 2,
+      deleteOnRollback: false,
+      shallowClone: true
+    },
+    staging: {
+      servers: {
+        host: 'beta.utnianos.com.ar',
+        user: config.staging.user,
+      },
+      key: config.staging.sshKey
+    }
+  });
+};
