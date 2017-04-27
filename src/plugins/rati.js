4886f5b4070d8ab66eebe9759bedca42850de06c
Javier Rotelli <javier.rotelli@gmail.com>
2017-04-27 00:19:19 -0300

    arreglando el ratibot

--- src/plugins/rati.js
+++ src/plugins/rati.js
@@ -1,10 +1,17 @@
-export default (config, emitter) => {
+export default (config, emitter, log) => {
   emitter.on('reaction:added', (payload) => {
     if (payload.item.type === 'message' &&
       payload.item.channel === config.channel &&
       payload.item_user === config.cook) {
-      const time = new Date(payload.item.event_ts).getHours()
-      if (time >= 10 && time < 14) {
+      const messageTime = new Date(payload.item.ts * 1000)
+      messageTime.setHours(0, 0, 0, 0)
+
+      const reactionTime = new Date(payload.ts * 1000)
+      reactionTime.setHours(0, 0, 0, 0)
+
+      log({messageTime, reactionTime})
+
+      if (reactionTime > messageTime) {
         const text = `<@${payload.item.user}> \r\n https://media.giphy.com/media/26uf3DbP1a8nwAw6c/giphy.gif`
         emitter.emit('send:message', text, payload.item.channel)
       }
---
+++ test/ratiTest.js
@@ -0,0 +1,57 @@
+/* eslint-env mocha */
+
+import sinon from 'sinon'
+const EventEmitter = require('events').EventEmitter
+import createDebug from 'debug'
+
+import rati from '../src/plugins/rati'
+
+const log = createDebug('taperbot:core')
+
+const config = {
+  channel: 'G06E6NZ89',
+  cook: 'U0XP25ZMJ'
+}
+
+const getReactionPayload = (messageTime, reactionTime) => {
+  return {
+    type: 'reaction_added',
+    user: 'U0XP25ZMJ',
+    item: {
+      type: 'message',
+      channel: 'G06E6NZ89',
+      ts: messageTime.getTime() / 1000
+    },
+    reaction: 'the_horns::skin-tone-3',
+    item_user: 'U0XP25ZMJ',
+    event_ts: reactionTime.getTime() / 1000,
+    ts: reactionTime.getTime() / 1000
+  }
+}
+
+describe('Test rati plugin', function () {
+  it('should send a message when the days differ', function (done) {
+    const emitter = new EventEmitter()
+    const spy = sinon.spy()
+
+    emitter.on('send:message', spy)
+
+    rati(config, emitter, log)
+    emitter.emit('reaction:added', getReactionPayload(new Date(2017, 3, 2), new Date(2017, 3, 3)))
+    sinon.assert.calledOnce(spy)
+    done()
+  })
+
+  it('should not send a message when the days are equal', function (done) {
+    const emitter = new EventEmitter()
+    const spy = sinon.spy()
+
+    emitter.on('send:message', spy)
+
+    rati(config, emitter, log)
+    emitter.emit('reaction:added', getReactionPayload(new Date(2017, 3, 2), new Date(2017, 3, 2)))
+    sinon.assert.notCalled(spy)
+    done()
+  })
+})
+
