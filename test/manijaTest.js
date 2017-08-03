be90b9a1aaf921319fc912acd514ae1772100180
Javier Rotelli <javier.rotelli@gmail.com>
2017-08-03 14:45:25 -0300

    test para asegurarnos que el trolleo funcione

--- test/manijaTest.js
+++ test/manijaTest.js
@@ -7,7 +7,7 @@ const expect = chai.expect
 
 describe('Test getLetsPlayFriendlyMessage', function () {
   it('should return a message for multiple rivals', function (done) {
-    const rivals = '@jarro, @ema'
+    const rivals = '@jarro(v), @ema(l)'
     const result = getLetsPlayFriendlyMessage(rivals)
 
     expect(result).to.be.a('string')
@@ -15,10 +15,19 @@ describe('Test getLetsPlayFriendlyMessage', function () {
   })
 
   it('should return a message for one rival', function (done) {
-    const rivals = '@jarro'
+    const rivals = '@jarro(l)'
     const result = getLetsPlayFriendlyMessage(rivals)
 
     expect(result).to.be.a('string')
+    done()
+  })
+
+  it('should return a custom message when the rival is aspero', function (done) {
+    const rivals = '@aspero(v)'
+    const result = getLetsPlayFriendlyMessage(rivals)
+
+    expect(result).to.be.a('string')
+    expect(result).to.include('gluteos')
     done()
   })
 
