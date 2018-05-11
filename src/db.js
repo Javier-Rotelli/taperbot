76b37ffd4f9457e92193fb370845b320ed012026
Javier Rotelli <javier.rotelli@gmail.com>
2018-05-11 14:40:37 -0300

    persitencia agregada, falta testear bien

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ import createDebug from 'debug'
 
 const log = createDebug('taperbot:core:db')
 
-export const getDbInstance = dbPath => pluginName => {
+export const getDbInstance = dbPath => pluginName => () => {
   log(`starting DB for ${pluginName}`)
   return new Promise((resolve, reject) => {
     level(`${dbPath}${pluginName}.db`, {}, function (err, db) {
