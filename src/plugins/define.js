fe1e5f3bcb5521470882d68f681e8c494d0dfd2f
Javier Rotelli <javier.rotelli@gmail.com>
2019-01-29 11:52:25 -0300

    agrego plugin de definiciones

---
+++ src/plugins/define.js
@@ -0,0 +1,34 @@
+import { create } from 'apisauce'
+import { take, forEach, propOr, compose, map, ifElse, lt, length } from 'ramda'
+import commandParser from '../commandParser'
+
+export default (config, emitter, log) => {
+  // define the api
+  const api = create({
+    baseURL: 'http://api.urbandictionary.com/v0/define',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  })
+
+  emitter.on('received:message', (message) => {
+    const command = commandParser(message.text)
+    if (command === null || command.command !== 'define') {
+      return
+    }
+
+    api.get('', {term: command.text}).then(res => {
+      compose(
+        forEach(answer => emitter.emit('send:message', answer, message.channel)),
+        ifElse(
+          compose(lt(0), length),
+          map(def => `${def.definition} (${def.permalink})`),
+          () => ['I\'m sorry, my responses are limited. You must ask the right question. \r\n https://pics.me.me/m-sorry-my-responses-are-limited-you-must-ask-the-right-32270832.png']
+        ),
+        take(2),
+        propOr([], 'list')
+      )(res.data)
+    }).catch(err => { throw err })
+  })
+}
