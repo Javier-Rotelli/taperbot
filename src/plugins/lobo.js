a01a6d6c270f177e87fc40ede14d4b3164b0fb53
Javier Rotelli <javier.rotelli@gmail.com>
2022-05-05 19:25:34 -0300

    lobobot

---
+++ src/plugins/lobo.js
@@ -0,0 +1,24 @@
+import { keys } from "ramda";
+import eventTypes from "../eventTypes";
+
+const forwardTemplate = (channel, user, text) =>
+  `${channel} -> ${user}:
+    ${text}
+`;
+
+export default (config, emitter, log) => {
+  emitter.on(eventTypes.IN.receivedOtherMessage, (message) => {
+    if (!keys(config.channels).includes(message.channel)) {
+      return;
+    }
+
+    emitter.emit(
+      eventTypes.OUT.sendMessage,
+      forwardTemplate(
+        config.channels[message.channel],
+        `<@${message.user}>`,
+        message.text
+      )
+    );
+  });
+};
