0cab7b380b18bea831fc2b509ffb32fa825ac9b7
Javier Rotelli <javier.rotelli@gmail.com>
2022-05-07 17:47:47 -0300

    use nvm to force node version

--- shipitfile.js
+++ shipitfile.js
@@ -1,38 +1,35 @@
-var pkg = require('./package.json')
+var pkg = require("./package.json");
 
 module.exports = function (shipit) {
-  require('shipit-deploy')(shipit)
-  require('shipit-shared')(shipit)
-  require('shipit-yarn')(shipit)
-  require('shipit-pm2')(shipit)
-  var config = require('./shipitfile.config.json')
+  require("shipit-deploy")(shipit);
+  require("shipit-shared")(shipit);
+  require("shipit-nvm")(shipit);
+  require("shipit-yarn")(shipit);
+  require("shipit-pm2")(shipit);
+  var config = require("./shipitfile.config.json");
 
   shipit.initConfig({
     default: {
-      workspace: '/tmp/taperbot',
-      deployTo: '/home/taperbot/code',
+      workspace: "/tmp/taperbot",
+      deployTo: "/home/taperbot/code",
       repositoryUrl: pkg.repository.url,
-      branch: 'master',
-      ignores: ['.git'],
+      branch: "master",
+      ignores: [".git"],
       keepReleases: 2,
       deleteOnRollback: false,
       shallowClone: true,
       shared: {
         overwrite: true,
-        dirs: [
-          'data'
-        ],
-        files: [
-          'config.yml'
-        ]
-      }
+        dirs: ["data"],
+        files: ["config.yml"],
+      },
     },
     staging: {
       servers: {
         host: config.staging.host,
-        user: config.staging.user
+        user: config.staging.user,
       },
-      key: config.staging.sshKey
-    }
-  })
-}
+      key: config.staging.sshKey,
+    },
+  });
+};
