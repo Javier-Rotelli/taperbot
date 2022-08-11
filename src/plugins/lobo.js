ac9b3c8b29204af6cdd1cc1af6b421c00e83faec
Emanuel <emanuel@aijunasys.com.ar>
2022-08-11 18:38:20 -0300

    Mejora formato del lobo

--- src/plugins/lobo.js
+++ src/plugins/lobo.js
@@ -1,22 +1,24 @@
 import { keys } from "ramda";
 import eventTypes from "../eventTypes";
 
-const forwardTemplate = (channel, user, text) =>
-  `${channel} -> ${user}:
-    ${text}
-`;
+const forwardTemplate = (channel, user, text) =>({
+  text: `*${channel} ~ <@${user}>:*
+> ${text.replace(/\n/g, "\n> ")}`
+});
 
 export default (config, emitter, log) => {
   emitter.on(eventTypes.IN.receivedOtherMessage, (message) => {
     if (!keys(config.channels).includes(message.channel)) {
       return;
     }
-
+    if (!message.text) {
+      return;
+    }
     emitter.emit(
       eventTypes.OUT.sendMessage,
       forwardTemplate(
         config.channels[message.channel],
-        `<@${message.user}>`,
+        message.user,
         message.text
       ),
       config.platea
