5e85f675318792a0dbf87aef0fe184c5e8161037
Patricio Pitaluga <patricio.pitaluga@gmail.com>
2023-01-09 22:22:52 -0300

    2023 calendar and fix cli

--- src/plugins/proximoferiado.js
+++ src/plugins/proximoferiado.js
@@ -25,6 +25,12 @@ const weekDaysLocale = [
   "sábado",
 ];
 
+const ENUM_TYPES = {
+  INAMOVIBLE: 'inamovible',
+  NO_LABORABLE: 'no laborable',
+  TRASLADABLE: 'trasladable',
+};
+
 import commandParser from "../commandParser";
 
 /** @type { import("./plugin").TaperbotPlugin } */
@@ -38,148 +44,129 @@ export default ({ config, emitter, log }) => {
     // const theDate = ((_theDateAsString) ? dayjs(_theDateAsString).startOf('day') : dayjs().startOf('day'));
     const theDate = dayjs().startOf("day");
 
-    const feriados2022 = [
+    const holidaysThisYear = [
       {
-        date: "2022-01-01",
-        name: "Año nuevo",
-        type: "inamovible",
-        weekday: "sábado",
+        date: '2023-01-01',
+        name: 'Año Nuevo',
+        type: ENUM_TYPES.INAMOVIBLE,
       },
       {
-        date: "2022-02-28",
-        name: "Feriado de carnaval",
-        type: "inamovible",
-        weekday: "lunes",
-        nextTo:
-          "4 días no laborables, desde el sábado 26 de febrero hasta el martes 1 de marzo",
+        date: '2023-02-20',
+        name: 'Carnaval',
+        type: ENUM_TYPES.INAMOVIBLE,
+        nextTo: '4 días no laborables, desde el sábado 18 de febrero hasta el martes 21 de febrero',
       },
       {
-        date: "2022-03-01",
-        name: "Feriado de carnaval",
-        type: "inamovible",
-        weekday: "martes",
-        nextTo:
-          "4 días no laborables, desde el sábado 26 de febrero hasta el martes 1 de marzo",
+        date: '2023-02-21',
+        name: 'Carnaval',
+        type: ENUM_TYPES.INAMOVIBLE,
+        nextTo: '4 días no laborables, desde el sábado 18 de febrero hasta el martes 21 de febrero',
       },
       {
-        date: "2022-03-24",
-        name: "Día Nacional de la Memoria por la Verdad y la Justicia",
-        type: "inamovible",
-        weekday: "jueves",
+        date: '2023-03-24',
+        name: 'Día Nacional de la Memoria por la Verdad y la Justicia',
+        type: ENUM_TYPES.INAMOVIBLE,
+        nextTo: '3 días no laborables, desde el viernes 24 de marzo hasta el domingo 26 de marzo',
       },
       {
-        date: "2022-04-02",
-        name: "Día del Veterano y de los Caídos en la Guerra de Malvinas",
-        type: "inamovible",
-        weekday: "sábado",
+        date: '2023-04-02',
+        name: 'Día del Veterano y de los Caídos en la Guerra de Malvinas',
+        type: ENUM_TYPES.INAMOVIBLE,
       },
       {
-        date: "2022-04-14",
-        name: "Jueves Santo",
-        type: "no laborable",
-        weekday: "jueves",
-        nextTo: "4 días no laborables, desde el 14 hasta el 17 inclusive",
+        date: '2023-04-06',
+        name: 'Jueves Santo Festividad Cristiana',
+        type: ENUM_TYPES.NO_LABORABLE,
+        nextTo: '4 días no laborables, desde el jueves 6 de abril hasta el domingo 9 de abril',
       },
       {
-        date: "2022-04-15",
-        name: "Viernes Santo",
-        type: "inamovible",
-        weekday: "viernes",
-        nextTo: "4 días no laborables, desde el 14 hasta el 17 inclusive",
+        date: '2023-04-07',
+        name: 'Viernes Santo Festividad Cristiana',
+        type: ENUM_TYPES.INAMOVIBLE,
+        nextTo: '4 días no laborables, desde el jueves 6 de abril hasta el domingo 9 de abril',
       },
       {
-        date: "2022-05-01",
-        name: "Día del trabajador",
-        type: "inamovible",
-        weekday: "domingo",
+        date: '2023-05-01',
+        name: 'Día del Trabajador',
+        type: ENUM_TYPES.INAMOVIBLE,
+        nextTo: '3 días no laborables, desde el viernes 29 de abril hasta el lunes 1 de mayo',
       },
       {
-        date: "2022-05-25",
-        name: "Día de la Revolución de Mayo",
-        type: "inamovible",
-        weekday: "miércoles",
+        date: '2023-05-25',
+        name: 'Día de la Revolución de Mayo',
+        type: ENUM_TYPES.INAMOVIBLE,
+        nextTo: '4 días no laborables, desde el jueves 25 de mayo hasta el domingo 28 de mayo',
       },
       {
-        date: "2022-06-17",
-        name: "Paso a la inmortalidad del Gral. Martín Miguel de Güemes",
-        type: "trasladable",
-        weekday: "viernes",
-        nextTo:
-          "4 días no laborables, desde el viernes 17 hasta el lunes 20 inclusive",
+        date: '2023-05-26',
+        name: 'Feriado con fines turísticos',
+        nextTo: '4 días no laborables, desde el jueves 25 de mayo hasta el domingo 28 de mayo',
       },
       {
-        date: "2022-06-20",
-        name: "Paso a la Inmortalidad del Gral. Manuel Belgrano",
-        type: "inamovible",
-        weekday: "lunes",
-        nextTo:
-          "4 días no laborables, desde el viernes 17 hasta el lunes 20 inclusive",
+        date: '2023-06-17',
+        name: 'Paso a la Inmortalidad del Gral. Don Martín Miguel de Güemes',
+        type: ENUM_TYPES.TRASLADABLE,
+        nextTo: '4 días no laborables, desde el sábado 17 de junio hasta el martes 20 de junio',
       },
       {
-        date: "2022-07-09",
-        name: "Día de la Independencia",
-        type: "inamovible",
-        weekday: "sábado",
+        date: '2023-06-19',
+        name: 'Feriado con fines turísticos',
+        nextTo: '4 días no laborables, desde el sábado 17 de junio hasta el martes 20 de junio',
       },
       {
-        date: "2022-08-15",
-        name: "Paso a la inmortalidad del Gral. José de San Martín",
-        movedFrom: "17 de agosto",
-        type: "trasladable",
-        weekday: "lunes",
-        nextTo:
-          "3 días no laborables, desde el sábado 13, hasta el lunes 15 inclusive",
+        date: '2023-06-20',
+        name: 'Paso a la Inmortalidad del Gral. Manuel Belgrano',
+        type: ENUM_TYPES.INAMOVIBLE,
+        nextTo: '4 días no laborables, desde el sábado 17 de junio hasta el martes 20 de junio',
       },
       {
-        date: "2022-10-07",
-        name: "Feriado con fines turísticos",
-        weekday: "viernes",
-        nextTo:
-          "4 días no laborables, desde el viernes 7 hasta el lunes 10 inclusive",
+        date: '2023-07-09',
+        name: 'Día de la Independencia ',
+        type: ENUM_TYPES.INAMOVIBLE,
       },
       {
-        date: "2022-10-10",
-        name: "Día del Respeto a la Diversidad Cultural",
-        movedFrom: "12 de octubre",
-        type: "trasladable",
-        weekday: "lunes",
-        nextTo:
-          "4 días no laborables, desde el viernes 7 hasta el lunes 10 inclusive",
+        date: '2023-08-21',
+        name: 'Paso a la Inmortalidad del Gral. José de San Martín (17/8)',
+        type: ENUM_TYPES.TRASLADABLE,
+        nextTo: '3 días no laborables, desde el sábado 19 de agosto hasta el lunes 21 de agosto',
       },
       {
-        date: "2022-11-20",
-        name: "Día de la Soberanía Nacional",
-        type: "trasladable",
-        weekday: "domingo",
-        nextTo: "3 días no laborables, hasta el lunes 21 inclusive",
+        date: '2023-10-13',
+        name: 'Feriado con fines turísticos',
+        nextTo: '4 días no laborables, desde el viernes 13 de octubre hasta el lunes 16 de octubre',
       },
       {
-        date: "2022-11-21",
-        name: "Feriado con fines turísticos",
-        weekday: "lunes",
-        nextTo: "3 días no laborables, hasta el lunes 21 inclusive",
+        date: '2023-10-16',
+        name: 'Día del Respeto a la Diversidad Cultural (12/10)',
+        type: ENUM_TYPES.TRASLADABLE,
+        nextTo: '4 días no laborables, desde el viernes 13 de octubre hasta el lunes 16 de octubre',
       },
       {
-        date: "2022-12-08",
-        name: "Inmaculada Concepción de María",
-        type: "inamovible",
-        weekday: "jueves",
+        date: '2023-11-20',
+        name: 'Día de la Soberanía Nacional',
+        type: ENUM_TYPES.TRASLADABLE,
+        nextTo: '3 días no laborables, desde el sábado 18 de noviembre hasta el lunes 20 de noviembre',
       },
       {
-        date: "2022-12-09",
-        name: "Feriado con fines turísticos",
-        weekday: "viernes",
-        nextTo: "4 días no laborables, desde el jueves 8 hasta el domingo 11",
+        date: '2023-12-08',
+        name: 'Inmaculada Concepción de María',
+        type: ENUM_TYPES.INAMOVIBLE,
+        nextTo: '3 días no laborables, desde el viernes 8 de diciembre hasta el domingo 10 de diciembre',
       },
       {
-        date: "2022-12-25",
-        name: "Navidad",
-        type: "inamovible",
-        weekday: "domingo",
+        date: '2023-12-25',
+        name: 'Navidad',
+        type: ENUM_TYPES.INAMOVIBLE,
+        nextTo: '3 días no laborables, desde el sabado 23 de diciembre hasta el lunes 25 de diciembre',
+      },
+      {
+        date: '2024-01-01',
+        name: 'Año nuevo',
+        type: ENUM_TYPES.INAMOVIBLE,
       },
     ];
 
-    const nextFeriado = feriados2022.find(
+    const nextFeriado = holidaysThisYear.find(
       (_) =>
         _.date === theDate.format("YYYY-MM-DD") ||
         dayjs(_.date).isAfter(theDate)
@@ -205,14 +192,6 @@ export default ({ config, emitter, log }) => {
       if (nextFeriado.nextTo)
         responseText +=
           " Puede conformar un grupo de " + nextFeriado.nextTo + ".";
-
-      responseText +=
-        " [Agregar a Google Calendar](https://www.google.com/calendar/render?action=TEMPLATE&text=" +
-        nextFeriado.name.replace(/ /g, "%20") +
-        "&dates=" +
-        theFeriadoDate.format("YYYYMMDD") +
-        "%2F" +
-        theFeriadoDate.add(1, "days").format("YYYYMMDD");
 
       responseText = responseText.replace("en *1 día*", "*mañana*");
 
--- taperbot-cli.js
+++ taperbot-cli.js
@@ -3,11 +3,10 @@ import { getConfig } from "./src/config";
 import eventTypes from "./src/eventTypes";
 import createDebug from "debug";
 import adminPlugin from "./src/plugins/admin";
+import createStorage from "./src/modules/storage";
 
 import { EventEmitter } from "events";
 const emitter = new EventEmitter();
-
-const conf = getConfig();
 
 process.env.DEBUG = "*";
 
@@ -19,22 +18,21 @@ for (let c = 2; c < process.argv.length; c++) {
   space = " ";
 }
 
-const initPlugins = (plugins, emitter) => {
-  const pluginsFolder = "./src/plugins/";
-
-  return Object.keys(plugins)
-    .map((pluginName) => {
-      const pluginConfig = plugins[pluginName];
-      return require(`${pluginsFolder}${pluginName}`).default(
-        pluginConfig,
-        emitter,
-        createDebug(`taperbot:${pluginName}`)
-      );
+const config = getConfig();
+const pluginsFolder = "./src/plugins/";
+Object.keys(config.plugins)
+  .map((pluginName) => {
+    const pluginConfig = config.plugins[pluginName];
+    return [pluginName, require(`${pluginsFolder}${pluginName}`).default, pluginConfig];
+  }).concat([["admin", adminPlugin, config]]).forEach(([pluginName, plugin, config]) => {
+    const log = createDebug(`taperbot:${pluginName}`);
+    plugin({
+      config,
+      emitter,
+      log,
+      storage: createStorage(pluginName, { log }),
     })
-    .concat([adminPlugin(conf, emitter, createDebug("taperbot:admin"))]);
-};
-
-initPlugins(conf.plugins, emitter);
+  });
 
 emitter.emit(eventTypes.IN.receivedMessage, {
   type: "message",
