9bafb2056dd2a492a12ac3e6d6b90785fc223640
Javier Rotelli <javier.rotelli@gmail.com>
2023-05-12 16:26:34 -0300

    use config to get the api key for aiudame

--- src/plugins/aiudame.js
+++ src/plugins/aiudame.js
@@ -1,9 +1,5 @@
 import commandParser from "../commandParser";
-import { Configuration, OpenAIApi } from 'openai';
-const openai = new OpenAIApi(new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-}));
-if (!process.env.OPENAI_API_KEY) console.log('OPENAI_API_KEY env variable not set. AIudame plugin disabled.');
+import { Configuration, OpenAIApi } from "openai";
 
 /**
  * Taperbot plugin to anwser technical questions using GPT3 from OpenAI.
@@ -13,29 +9,41 @@ if (!process.env.OPENAI_API_KEY) console.log('OPENAI_API_KEY env variable not se
  * Test it from terminal: npm run cli /aiudame how do I set a public folder for an express server
  **/
 export default ({ config, emitter, log }) => {
+  const openai = new OpenAIApi(
+    new Configuration({
+      apiKey: config.apiKey,
+    })
+  );
+
   emitter.on("received:message", (message) => {
     const command = commandParser(message.text);
     if (command === null || command.command !== "aiudame") {
       return;
     }
-    if (!process.env.OPENAI_API_KEY) {
-      emitter.emit("send:message", 'AIudame no está disponible en este momento.', message.channel);
-    }
 
-    const prompt = message.text.substr(message.text.indexOf('/aiudame ') + '/aiudame '.length);
+    const prompt = message.text.substr(
+      message.text.indexOf("/aiudame ") + "/aiudame ".length
+    );
 
-    openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt: `Assume that I am a software engineer asking you this technical question:
-${prompt + ((prompt.endsWith('.')) ? '' : '.')}
+    openai
+      .createCompletion({
+        model: "text-davinci-003",
+        prompt: `Assume that I am a software engineer asking you this technical question:
+${prompt + (prompt.endsWith(".") ? "" : ".")}
 Provide a code example if needed.
 `,
-      max_tokens: 175,
-    })
+        max_tokens: 175,
+      })
       .then((_) => {
         // console.log(_.data.choices[0].text.trim());
-        emitter.emit("send:message", _.data.choices[0].text.trim(), message.channel);
+        emitter.emit(
+          "send:message",
+          _.data.choices[0].text.trim(),
+          message.channel
+        );
       })
-      .catch((_) => { throw _; });
+      .catch((_) => {
+        throw _;
+      });
   });
 };
